2fa79c2a18c6f60d5bf769ded7d42af9bddefe97
thomst08 <thomst08@hotmail.com>
2023-02-06 13:55:00 +1100

    Updated program to be a service

--- WeighBridgeReader/WeighBridgeReader/Program.cs
+++ WeighBridgeReader/WeighBridgeReader/Program.cs
@@ -7,6 +7,10 @@ namespace WeighBridgeReader
         public static void Main(string[] args)
         {
             IHost host = Host.CreateDefaultBuilder(args)
+                .UseWindowsService(options =>
+                {
+                    options.ServiceName = "Weighbridge Weight Logger";
+                })
                 .ConfigureServices(services =>
                 {
                     services.AddHostedService<Worker>();
